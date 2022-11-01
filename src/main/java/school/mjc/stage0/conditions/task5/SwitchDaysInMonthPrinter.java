8b8a483141523fa25c4aaa0d0e26425f17057a3b
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-11-01 09:22:58 +0500

    Switch

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -4,21 +4,29 @@ public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
         switch (month){
             case 1:
+                System.out.println(31); break;
             case 3:
+                System.out.println(31); break;
             case 5:
+                System.out.println(31); break;
             case 7:
+                System.out.println(31); break;
             case 9:
+                System.out.println(31); break;
             case 11:
-                System.out.println(31);
+                System.out.println(31); break;
             case 2:
-                System.out.println(28);
+                System.out.println(28); break;
             case 4:
+                System.out.println(30); break;
             case 6:
+                System.out.println(30); break;
             case 8:
+                System.out.println(30); break;
             case 10:
-                System.out.println(30);
+                System.out.println(30); break;
             default:
-                System.out.println("wrong number!");
+                System.out.println("wrong number!"); break;
         }
     }
 }
